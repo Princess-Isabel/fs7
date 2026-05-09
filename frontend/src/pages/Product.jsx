7474faf84056246bea8e783b653117c336f99e9b
Princess-Isabel <isabelheart15@gmail.com>
2026-05-09 14:16:10 +0800

    products

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -10,7 +10,7 @@ const Products = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get(`${BASE_URL}products`);
+      const response = await axios.get(`${BASE_URL}/products`);
       setProducts(response.data);
       setLoading(false);
     } catch (err) {
